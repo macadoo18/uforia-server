d5693f15152ce3284cb6ee3dd4f3a4d4e44fb2a1
Mac Hale <machaledesign@gmail.com>
2021-12-06 19:17:36 -0700

    edits config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  NODE_ENV: process.env.NODE_ENV || "production",
+  NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 8080,
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   DATABASE_URL:
