bb733c54c58ff2448531f2ece34db7ff71758ac6
Mac Hale <machaledesign@gmail.com>
2021-12-15 19:24:16 -0700

    edits ssl in server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const knex = require("knex");
-const app = require("./app");
-const { PORT, TEST_DATABASE_URL, DATABASE_URL } = require("./config");
+const knex = require('knex');
+const app = require('./app');
+const { PORT, TEST_DATABASE_URL, DATABASE_URL } = require('./config');
 
 const db = knex({
-  client: "pg",
-  connection:
-    process.env.NODE_ENV === "test" ? TEST_DATABASE_URL : DATABASE_URL,
-  SSL: true
+  client: 'pg',
+  connection: process.env.NODE_ENV === 'test' ? TEST_DATABASE_URL : DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-app.set("db", db);
+app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`server listening at http://localhost:${PORT}`);
