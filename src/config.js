6f4960e5795cb405b3d3c8b8c8d69424d83a4db6
Mac Hale <machaledesign@gmail.com>
2021-12-21 18:53:15 -0700

    adds console for database

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
+console.log('DATABASE_URL', process.env.DATABASE_URL);
 module.exports = {
-  NODE_ENV: process.env.NODE_ENV || "development",
+  NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 8080,
-  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   DATABASE_URL:
-    process.env.DATABASE_URL ||
-    "postgres://habituser:habit1234@localhost/habitapp",
+    process.env.DATABASE_URL || 'postgres://habituser:habit1234@localhost/habitapp',
 
   TEST_DATABASE_URL:
     console.log(process.env.TEST_DATABASE_URL) ||
     process.env.TEST_DATABASE_URL ||
-    "postgres://habituser:habit1234@localhost/habitapp_test",
+    'postgres://habituser:habit1234@localhost/habitapp_test',
 };
