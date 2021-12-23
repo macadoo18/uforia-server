1bfa8e6f436f76f374f2bb18e87b9f5f5fc37d6c
Mac Hale <machaledesign@gmail.com>
2021-12-23 11:29:43 -0700

    ssl configs

--- src/server.js
+++ src/server.js
@@ -6,7 +6,8 @@ const { PORT, TEST_DATABASE_URL, DATABASE_URL } = require('./config');
 
 const db = knex({
   client: 'pg',
-  connection: process.env.NODE_ENV === 'test' ? TEST_DATABASE_URL : DATABASE_URL,
+  connection:
+    process.env.NODE_ENV === 'test' ? TEST_DATABASE_URL : DATABASE_URL + '?ssl=true',
 });
 
 app.set('db', db);
